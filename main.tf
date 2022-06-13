dae1e8acaa44e339aeda11b1f8a5a81c0e6de223
emadam <ldy9037@naver.com>
2022-06-13 19:51:14 +0900

    Project Setup

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.20.0"
+    }
+  }
+
+  backend "gcs" {}
+
+  required_version = ">= 1.1.7"
+}
+
+provider "google" {
+  region = var.region
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "GCP Region"
+  type        = string
+}
+
+variable "project_id" {
+  description = "Project ID"
+  type        = string
+}
